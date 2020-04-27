3a03256e04aa5a8cdde3ec60b8c1a6544c7de00f
Ariel Weinberger <weinberger.ariel@gmail.com>
2020-04-27 18:45:52 +0200

    First e-mail (test mail)

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -1,8 +1,32 @@
+import AWS from 'aws-sdk';
+
+const ses = new AWS.SES({ region: 'eu-west-1' });
+
 async function sendMail(event, context) {
-  console.log(event);
-  return event;
+  const params = {
+    Source: 'ariel@codingly.io',
+    Destination: {
+      ToAddresses: ['ariel@codingly.io'],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: 'Hello from Codingly!',
+        },
+      },
+      Subject: {
+        Data: 'Test Mail',
+      },
+    },
+  };
+
+  try {
+    const result = await ses.sendEmail(params).promise();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export const handler = sendMail;
-
-
