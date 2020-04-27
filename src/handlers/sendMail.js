4e80ae8dfc7e55c183212026e5dee262ffe3aa6f
Ariel Weinberger <weinberger.ariel@gmail.com>
2020-04-27 19:29:55 +0200

    Provision MailQueue, IAM and sendMail

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -3,19 +3,25 @@ import AWS from 'aws-sdk';
 const ses = new AWS.SES({ region: 'eu-west-1' });
 
 async function sendMail(event, context) {
+  const record = event.Records[0];
+  console.log('record processing', record);
+
+  const email = JSON.parse(record.body);
+  const { subject, body, recipient } = email;
+
   const params = {
     Source: 'ariel@codingly.io',
     Destination: {
-      ToAddresses: ['ariel@codingly.io'],
+      ToAddresses: [recipient],
     },
     Message: {
       Body: {
         Text: {
-          Data: 'Hello from Codingly!',
+          Data: body,
         },
       },
       Subject: {
-        Data: 'Test Mail',
+        Data: subject,
       },
     },
   };
